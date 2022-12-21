f1635c27d8dc9c55139ee40ec56158bcab684a31
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2022-12-20 20:13:38 -0500

    Update long_mpc.py

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -54,7 +54,7 @@ T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.7
 MAX_ACCEL = 2.0
 T_FOLLOW = 1.
-COMFORT_BRAKE = 2.
+COMFORT_BRAKE = 3.3
 STOP_DISTANCE = 3.0
 
 def get_stopped_equivalence_factor(v_lead):
