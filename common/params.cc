50fe84e0f17c91408a761db3930571bb677c56fa
xps-genesis <aruncool17@gmail.com>
2023-05-28 11:46:26 -0400

    xps update

--- common/params.cc
+++ common/params.cc
@@ -206,6 +206,9 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"VisionRadarToggle", PERSISTENT},
     {"WheeledBody", PERSISTENT},
+    {"ChryslerMangoLat", PERSISTENT},
+    {"ChryslerMangoLong", PERSISTENT},
+    {"ChryslerMadGas", PERSISTENT},
 };
 
 } // namespace
--- selfdrive/manager/manager.py
+++ selfdrive/manager/manager.py
@@ -45,7 +45,10 @@ def manager_init() -> None:
     ("HasAcceptedTerms", "0"),
     ("LanguageSetting", "main_en"),
     ("OpenpilotEnabledToggle", "1"),
-    ("LongitudinalPersonality", str(log.LongitudinalPersonality.standard)),
+    ("LkasFullRangeAvailable", "1"),
+    ("ChryslerMangoLong", "1"),
+    ("ChryslerMangoLat", "1"),
+    ("ChryslerMadGas", "1"),
   ]
   if not PC:
     default_params.append(("LastUpdateTime", datetime.datetime.utcnow().isoformat().encode('utf8')))
