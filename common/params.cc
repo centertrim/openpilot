afa2d8c5a8e05ba105d511a410331def4d9909e7
xps-genesis <aruncool17@gmail.com>
2023-07-29 11:09:58 -0400

    o

--- common/params.cc
+++ common/params.cc
@@ -206,6 +206,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"Version", PERSISTENT},
     {"VisionRadarToggle", PERSISTENT},
     {"WheeledBody", PERSISTENT},
+    {"LkasFullRangeAvailable", PERSISTENT},
     {"ChryslerMangoLat", PERSISTENT},
     {"ChryslerMangoLong", PERSISTENT},
     {"ChryslerMadGas", PERSISTENT},
